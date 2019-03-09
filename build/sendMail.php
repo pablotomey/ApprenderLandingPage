632e6d659279d5dd8f51e272e0b3f93f6bc4a692
PABLO.TP87 <pablo.tp87@gmail.com>
2019-03-09 00:52:17 -0300

    Prueba Petición Ajax
    Realizando Pruebas

--- build/sendMail.php
+++ build/sendMail.php
@@ -19,33 +19,33 @@
             "Mensaje: " . $mensaje;
 
 
-    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-try {
-    //Server settings
-    $mail->SMTPDebug = 0;                                 // Enable verbose debug output
-    $mail->isSMTP();                                      // Set mailer to use SMTP
-    $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
-    $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'pablo.tp87@gmail.com';             // SMTP username
-    $mail->Password = 'paylamilla';                       // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 587;                                    // TCP port to connect to
+    $mail = new PHPMailer(true);                          // Passing `true` enables exceptions
 
-    //Recipients
-    $mail->setFrom('pablo.tp87@gmail.com', $nombre);
-    $mail->addAddress('pablo.tp87@gmail.com');            // Add a recipient
-    $mail->addCC('team.apprender@gmail.com');
+    try {
+        //Server settings
+        $mail->SMTPDebug = 0;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = 'pablo.tp87@gmail.com';             // SMTP username
+        $mail->Password = 'paylamilla';                       // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to
 
-    //Content
-    $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'Petición de contacto';
-    $mail->Body    = $body;
-    $mail->CharSet = 'UTF-8';
+        //Recipients
+        $mail->setFrom('pablo.tp87@gmail.com', $nombre);
+        $mail->addAddress('pablo.tp87@gmail.com');            // Add a recipient
+        $mail->addCC('team.apprender@gmail.com');
 
-    $mail->send();
-    echo 'Message has been sent';
-} catch (Exception $e) {
-    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
-}
-    
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = 'Petición de contacto';
+        $mail->Body    = $body;
+        $mail->CharSet = 'UTF-8';
+
+        $mail->send();
+        echo 'enviado';
+    } catch (Exception $e) {
+        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+    }   
 ?>
